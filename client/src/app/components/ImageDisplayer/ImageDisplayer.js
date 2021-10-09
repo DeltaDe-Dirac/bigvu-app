c7a094b4a844e1ddfa7a745ea035a57c45256d55
Delta de Dirac <igorshevtsov@gmail.com>
2021-10-09 13:22:15 +0300

    refactor to use image on canvas

--- client/src/app/components/ImageDisplayer/ImageDisplayer.js
+++ client/src/app/components/ImageDisplayer/ImageDisplayer.js
@@ -1,22 +1,41 @@
 import styles from "./ImageDisplayer.module.scss";
-import React from "react";
+import React, { useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { setImageLoaded, selectStatus } from "../../../features/gallery/gallerySlice";
+import { setImageLoaded, selectStatus, selectIndex } from "../../../features/gallery/gallerySlice";
 import { Image } from "react-bootstrap";
 
-const ImageDisplayer = ({ src }) => {
+const ImageDisplayer = ({ src, width, height }) => {
+  // console.log(src);
+  const canvasRef = useRef();
+  const imageRef = useRef();
   const dispatch = useDispatch();
   const imageStatus = useSelector(selectStatus);
+  const selectedItem = useSelector(selectIndex);
+
+  const handleOnLoad = () => {
+    dispatch(setImageLoaded());
+    console.log(canvasRef);
+    let ctx = canvasRef.current.getContext("2d");
+    ctx.drawImage(imageRef.current, 10, 10);
+  };
 
   return (
     <>
-      <Image
-        className={imageStatus === "loading" ? styles.displayNone : ""}
-        src={src}
-        fluid
-        onLoad={() => dispatch(setImageLoaded())}
-      />
-      <div className={imageStatus === "loading" ? styles.loader : ""}></div>
+      <div className={imageStatus === "loading" ? styles.loader : ""}>
+        <canvas
+          ref={canvasRef}
+          className={
+            imageStatus === "loading" || selectedItem === -1 ? styles.displayTransparent : styles.displayBlueCanvas
+          }
+          width="640"
+          height="360"
+        >
+          Your browser does not support the HTML canvas tag.
+        </canvas>
+      </div>
+      <div>
+        <Image ref={imageRef} className={styles.displayNone} src={src} fluid onLoad={() => handleOnLoad()} />
+      </div>
     </>
   );
 };
