af33c3609db936fa561e173797c17e30bdfdface
Delta de Dirac <igorshevtsov@gmail.com>
2021-10-10 14:07:15 +0300

    cleanup

--- client/src/app/components/PageNotFound/PageNotFound.js
+++ client/src/app/components/PageNotFound/PageNotFound.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { Redirect } from "react-router";
+import React from "react";
 import { useHistory } from "react-router-dom";
 
 import { Nav } from "react-bootstrap/";
@@ -9,23 +8,12 @@ import { Container } from "react-bootstrap";
 
 export default function PageNotFound({ errCode, errText }) {
   const history = useHistory();
-  // const [redirectTo, setRedirectTo] = useState(null);
-
-  // if (redirectTo !== null) {
-  //   return <Redirect to={redirectTo}></Redirect>;
-  // }
-
-  // const img = process.env.PUBLIC_URL.concat(imgPath);
-
   return (
     <div className="pagenotfound-img">
       <Container>
         <h1>{errCode}</h1>
         <div className="notfound">
           <p className="text-uppercase">{errText}</p>
-          {/* <Nav.Link eventKey="/white" onSelect={(selectedKey) => setRedirectTo(selectedKey)}>
-            Home
-          </Nav.Link> */}
           <Nav.Link eventKey="back" onSelect={() => history.goBack()}>
             Back
           </Nav.Link>
